0b7aa5829822c8823e5aedc85b989bdc6dc51fe2
Balcells Edouard <balcellsedouard1@gmail.com>
2018-11-29 11:37:23 +0100

    tp finished

--- es2015/app.js
+++ es2015/app.js
@@ -97,9 +97,15 @@ class TripService {
 
             setTimeout( () => {
                 // ici l'exécution du code est asynchrone
+                const tripTrouve = Array.from(this.trips)
+                    .find((unTrip) => unTrip.name === tripName)
 
                 // TODO utiliser resolve et reject en fonction du résultat de la recherche
-
+                if(tripTrouve) {
+                    resolve(tripTrouve)
+                } else {
+                    reject(`No trip with name ${tripName}`)
+                }
             }, 2000)
        });
    }    
@@ -107,12 +113,12 @@ class TripService {
 
 class PriceService {
 
-    constructor(identifiant, prix) {
-        this.identifiant = identifiant
-        this.prix = prix
+    constructor() {
         // TODO Map of 2 trips
-        // 'paris' --> price = 100
-        // 'rio-de-janeiro' --> price = 800)
+        this.map1 = new Map();
+        this.map1.set('paris',100)
+        this.map1.set('rio-de-janeiro',800)
+
         // no price for 'nantes'
     }
 
@@ -123,9 +129,46 @@ class PriceService {
                     setTimeout( () => {
                         // ici l'exécution du code est asynchrone
 
+                        const prix = this.map1.get(tripId)
+
                         // TODO utiliser resolve et reject en fonction du résultat de la recherche
 
+                        if(prix) {
+                            resolve (prix)
+                        }else {
+                            reject(`No price for trip Id : ${tripId}`)
+                        }
                     }, 2000)
                });
     }
 }
+
+const tripService = new TripService();
+
+tripService.findByName('Paris')
+    .then(tripTrouve => tripTrouve.id)
+    .then(idTrip => console.log('id,', idTrip))
+    .catch(msgErr => console.log('oops', msgErr))
+
+
+const priceService = new PriceService();
+
+priceService.findPriceByTripId('paris')
+    .then(tripId => console.log('Price found : ', tripId))
+    .catch(msgErr => console.log('oops', msgErr))
+
+let test1 = new TripService();
+test1.findByName('Paris').then(t=>{
+    console.log(t.toString())
+})
+
+let test2 = new PriceService();
+test2.findPriceByTripId('paris').then(P=>{
+    console.log("Price found" +p)
+})
+
+test1.findByName('Paris').then(t => {
+    test2.findPriceByTripId(t.id).then(p =>{
+        console.log(p)
+    })
+})
